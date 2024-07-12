5731bc313ce7afd5aa6d89f435b603f0b7a9a251
tim <18652961108@163.com>
2024-07-12 20:56:50 +0800

    test

--- hello_foundry/src/myToken.sol
+++ hello_foundry/src/myToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MockERC20 is ERC20 {
+contract MyToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 2e9 ether);
     }
